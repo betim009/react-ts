fe6eb7bff1d312ea5ea71fe88a3d98fb7ddfaa2e
Alberto <betim009@gmail.com>
2023-09-25 14:46:53 -0300

    estados new

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const toolKit = [
@@ -14,9 +14,15 @@ function App() {
   ];
 
   const [index, setIndex] = useState(0);
+  const [tool, setTool] = useState(toolKit);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    console.log(input);
+  }, [input]); // queremos que o efeito seja executado toda vez que o estado input for alterado, então incluímos [input]
 
   const handleNextClick = () => {
-    if (index + 1 < toolKit.length) {
+    if (index + 1 < tool.length) {
       setIndex(index + 1);
     } else {
       setIndex(0); // Volta para o primeiro elemento
@@ -31,13 +37,35 @@ function App() {
     }
   }
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    setInput(target.value)
+  }
+
+  const handleClick = () => {
+    if (input) {
+      setTool([...tool, input]); // Adiciona a nova ferramenta à lista existente
+      setInput(''); // Reseta o valor do input após a adição da ferramenta
+    }
+  }
+
   return (
-    <>
+    <div>
       <h1>Caixa de ferramentas de uma Pessoa Desenvolvedora</h1>
-      <h2>{toolKit[index]}</h2>
+      <h2>{tool[index]}</h2>
       <button onClick={handlePreviousClick}>Anterior</button>
       <button onClick={handleNextClick}>Próximo</button>
-    </>
+
+      <p>Adicione uma nova ferramenta!</p>
+      <input
+        type="text"
+        value={input} // resetar o valor do input
+        onChange={(event) => handleChange(event)}
+      />
+      <button
+        onClick={handleClick}
+      >Cadastrar</button>
+    </div>
   );
 }
 
