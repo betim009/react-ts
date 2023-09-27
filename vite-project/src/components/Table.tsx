e2c301fdf63efd47bb15899063cea4435ef7bb1a
Alberto <betim009@gmail.com>
2023-09-26 23:47:15 -0300

    table finish

--- vite-project/src/components/Table.tsx
+++ vite-project/src/components/Table.tsx
@@ -1,27 +1,48 @@
-import Team from './Team'
-import data from '../data/data'
+import React, { useState } from 'react';
+import Team from './Team';
+import data from '../data/data';
 
 function Table() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredData, setFilteredData] = useState(data);
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const searchTerm = event.target.value;
+        setSearchTerm(searchTerm);
+
+        const filteredTeams = data.filter(team => team.time.toLowerCase().includes(searchTerm.toLowerCase()));
+        setFilteredData(filteredTeams);
+    }
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Time</th>
-                    <th>Pontos</th>
-                    <th>Vitórias</th>
-                    <th>Derrotas</th>
-                    <th>Empates</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((team, index) => (
-                    <Team
-                        key={index}
-                        team={team}
-                    />
-                ))}
-            </tbody>
-        </table>
+        <div>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearch}
+                placeholder="Filtrar por time"
+            />
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Time</th>
+                        <th>Pontos</th>
+                        <th>Vitórias</th>
+                        <th>Derrotas</th>
+                        <th>Empates</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredData.map((team, index) => (
+                        <Team
+                            key={index}
+                            team={team}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
