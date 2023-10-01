87a9c6681978350d049ff88aadf8113e017d783a
Alberto <betim009@gmail.com>
2023-10-01 19:36:00 -0300

    feat: form

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,20 +3,56 @@ import './App.css'
 
 
 function App() {
-  const [name, SetName] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [school, setSchool] = useState('Médio')
+  const [resume, setResume] = useState('');
 
   return (
-    <div> 
+    <div>
       <div className='firstContainer'>
-        <label>
-          Nome:
-          <input value={name} onChange={({target}) => SetName(target.value)} />
-        </label>
-        <h5>Olá {name}</h5>
+        <form action="">
+          <label>
+            Nome:
+            <input
+              value={name}
+              onChange={({ target }) => setName(target.value)}
+            />
+          </label>
 
-        <button onClick={() => SetName('')}>Apagar!</button>
+          <label>
+            E-mail
+            <input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+          </label>
+
+          <label>
+            Escolaridade
+            <select
+              value={school}
+              onChange={({ target }) => setSchool(target.value)}
+            >
+              <option value="Médio">Médio</option>
+              <option value="Superior">Superior</option>
+              <option value="Pós-graduação">Pós-graduação</option>
+            </select>
+          </label>
+
+          <label>
+            Resumo das experiências
+
+            <textarea
+              value={resume}
+              onChange={({ target }) => setResume(target.value)}
+            />
+          </label>
+
+          <button>Enviar</button>
+        </form>
       </div>
-    </div>
+    </div >
   )
 }
 
