c613d456155ba3f6e0bce10be61134a22c968037
Alberto <betim009@gmail.com>
2023-10-03 22:32:54 -0300

    handler generico

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,106 +3,117 @@ import './App.css'
 
 
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [school, setSchool] = useState('Médio')
-  const [resume, setResume] = useState('');
+  const [formInfo, setFormInfo] = useState({
+    name: '',
+    email: '',
+    school: '',
+    resume: '',
+  });
   const [terms, setTerms] = useState(false);
   const [error, setError] = useState(false);
 
   const resetForm = () => {
-    setName('');
-    setEmail('');
-    setSchool('Médio');
-    setResume('');
+    setFormInfo({
+      name: '',
+      email: '',
+      school: 'Médio',
+      resume: '',
+    });
   }
 
-  const validateEmail = (email:string) => {
+  const validateEmail = (email: string) => {
     // Expressão regular para validar o e-mail
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(String(email).toLowerCase());
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(String(email).toLowerCase());
   }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if(!validateEmail(email)) {
+    if (!validateEmail(formInfo.email)) {
       alert('Email Inválido')
     }
 
-    if (terms && validateEmail(email)) {
+    if (terms && validateEmail(formInfo.email)) {
       alert(
-        `Nome: ${name}\nemail: ${email}\nEscolaridade: ${school}\nExperiências: ${resume}`
+        `Nome: ${formInfo.name}\nemail: ${formInfo.email}\nEscolaridade: ${formInfo.school}\nExperiências: ${formInfo.resume}`
       );
       resetForm();
+      setTerms(false);
+      setError(false);
     } else {
       setError(true);
     }
   }
 
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement|HTMLSelectElement|HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormInfo({
+      ...formInfo,
+      [name]: value,
+    });
+  }
+
   return (
-    <div>
-      <div className='firstContainer'>
-        <form action="" onSubmit={(event) => handleSubmit(event)}>
-          <label>
-            Nome:
-            <input
-              required
-              value={name}
-              onChange={({ target }) => setName(target.value)}
-            />
-          </label>
-
-          <label>
-            E-mail
-            <input
-              required
-              value={email}
-              onChange={({ target }) => setEmail(target.value)}
-            />
-          </label>
-
-          <label>
-            Escolaridade
-            <select
-              value={school}
-              onChange={({ target }) => setSchool(target.value)}
-            >
-              <option value="Médio">Médio</option>
-              <option value="Superior">Superior</option>
-              <option value="Pós-graduação">Pós-graduação</option>
-            </select>
-          </label>
-
-          <label>
-            Resumo das experiências
-
-            <textarea
-              value={resume}
-              onChange={({ target }) => setResume(target.value)}
-            />
-          </label>
-
-          <label>
-            Aceito os termos e condições
-            <input
-              type="checkbox"
-              checked={terms}
-              onChange={() => setTerms((prevTerms) => !prevTerms)}
-            />
-          </label>
-
-          <button>Enviar</button>
-        </form>
-
-        {error && (
-          <h4>
-            Você precisa aceitar os termos e condições para poder enviar o
-            currículo
-          </h4>
-        )}
-      </div>
-    </div >
-  )
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome:
+          <input
+            required
+            name="name"
+            value={formInfo.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          E-mail:
+          <input
+            required
+            name="email"
+            value={formInfo.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Escolaridade:
+          <select
+            name="schooling"
+            value={formInfo.school}
+            onChange={handleChange}
+          >
+            <option value="Médio">Médio</option>
+            <option value="Superior">Superior</option>
+            <option value="Pós-graduação">Pós-graduação</option>
+          </select>
+        </label>
+        <label>
+          Resumo das experiências
+          <textarea
+            name="resume"
+            value={formInfo.resume}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Aceito os termos e condições
+          <input
+            type="checkbox"
+            checked={terms}
+            onChange={() => setTerms(!terms)}
+          />
+        </label>
+        <button>Enviar</button>
+      </form>
+      {error && (
+        <h4>
+          Você precisa aceitar os termos e condições para poder enviar o
+          currículo
+        </h4>
+      )}
+    </>
+  );
 }
 
 export default App
