2983c09625644d1756fc5df4d5e3d8f783e3d695
Alberto <betim009@gmail.com>
2023-10-05 09:51:01 -0300

    router

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CoffeeList from './pages/CoffeList';
 
 function App() {
   return (
-    <div> 
-      <h1>Hello World!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/coffees" element={<CoffeeList />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
