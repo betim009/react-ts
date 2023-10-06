1463fdda875c741622d5cf2adee6e99cd9c8afa9
Alberto <betim009@gmail.com>
2023-10-05 23:31:21 -0300

    rota dinamica

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import CoffeeList from './pages/coffe-list';
+import CoffeeList from './pages/coffee-list';
+import Coffee from './pages/coffee';
 import NavBar from './components/nav-bar';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/coffees" element={<CoffeeList />} />
+        <Route path="coffees" element={<CoffeeList />} />
+        <Route path="coffees/:coffee" element={<Coffee />} />
       </Routes>
     </>
   );
