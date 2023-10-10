11b03813499a5da1d9e57d0fe7d5ec9c1ce018ca
Alberto <betim009@gmail.com>
2023-10-10 20:54:36 -0300

    components

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Greeting from './componets'
 
 
 function App() {
   return (
-    <div> 
-      <h1>Hello World!</h1>
+    <div>
+      <Greeting 
+        name={'Alberto'}
+      />
+            <Greeting 
+        name={'Cláudio'}
+      />
     </div>
   )
 }
--- vite-project/src/componets/index.tsx
+++ vite-project/src/componets/index.tsx
@@ -1,1 +1,18 @@
-//
+type GrettingProps = {
+    name: string;
+};
+
+let registerNumber = 0;
+
+function Greeting({ name }: GrettingProps) {
+    registerNumber = registerNumber + 1;
+
+    return (
+        <>
+            <h1>{`Meu nome é: ${name}!`}</h1>
+            <h2>{`Meu número de registro é: ${registerNumber}!`}</h2>
+        </>
+    );
+}
+
+export default Greeting;
