5445917bce48d2742c778e1260e23352c5950692
Alberto <betim009@gmail.com>
2023-10-10 20:59:20 -0300

    css

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,7 +4,7 @@ import Greeting from './componets'
 
 function App() {
   return (
-    <div>
+    <div className='container'>
       <Greeting 
         name={'Alberto'}
       />
--- vite-project/src/componets/index.tsx
+++ vite-project/src/componets/index.tsx
@@ -8,10 +8,10 @@ function Greeting({ name }: GrettingProps) {
     registerNumber = registerNumber + 1;
 
     return (
-        <>
+        <div className="card">
             <h1>{`Meu nome é: ${name}!`}</h1>
             <h2>{`Meu número de registro é: ${registerNumber}!`}</h2>
-        </>
+        </div>
     );
 }
 
