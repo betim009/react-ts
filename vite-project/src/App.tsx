916fb53cc6312aeafeceb5bbd7bb60a719e041cf
Alberto <betim009@gmail.com>
2023-10-16 14:28:53 -0300

    feat: primeiros exemplos de test

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,8 +3,13 @@ import './App.css'
 
 function App() {
   return (
-    <div> 
-      <h1>Hello World!</h1>
+    <div className="App">
+      <label htmlFor="id-email">
+        Email
+        <input id="id-email" type="email" />
+      </label>
+      <input data-testid="id-send" type="button" value="Enviar" />
+      <input type="button" value="Voltar" />
     </div>
   )
 }
