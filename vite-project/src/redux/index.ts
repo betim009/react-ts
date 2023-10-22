e5f4a5a026277c4aed63907fc9a70b001e81b88c
Alberto <betim009@gmail.com>
2023-10-22 20:01:06 -0300

    feat: refatorando o reducer

--- vite-project/src/redux/index.ts
+++ vite-project/src/redux/index.ts
@@ -1,27 +1,10 @@
 import { legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import counterReducer from './reducer/counterReducer';
 
 
-type ActionType = {
-    type: string,
-    payload: number,
-}
 
-const INITIAL_STATE = {
-    count: 0, // altere aqui e veja no devtols
-};
 
-const reducer = (state = INITIAL_STATE, action: ActionType) => {
-    switch (action.type) {
-        case "INCREMENT_COUNTER": {
-            return {
-                count: state.count + action.payload,
-            };
-        }
-        default: return state;
-    }
-};
-
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(counterReducer, composeWithDevTools());
 
 export default store;
