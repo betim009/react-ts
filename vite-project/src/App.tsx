e3ac33dceb9ae9d87544cbf4931d5f8d1c593f38
Alberto <betim009@gmail.com>
2023-10-24 21:51:16 -0300

    feat: redux thunk

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,29 @@
-import './App.css'
-
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDogImage } from './redux/actions';
+import { Dispatch, ReduxState } from './types';
+import './App.css';
 
 function App() {
+  const rootState = useSelector((state: ReduxState) => state);
+  const dispatch: Dispatch = useDispatch();
+
+  if (rootState.isFetching) return <p>Carregando...</p>;
+
   return (
-    <div> 
-      <h1>Hello World!</h1>
+    <div>
+      <button
+        onClick={() => {
+          dispatch(fetchDogImage());
+        }}
+        type="button"
+      >
+        Novo Doguinho
+      </button>
+      {rootState.imageURL && (
+        <img src={rootState.imageURL} alt="Imagem de um cachorro aleatório" />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
