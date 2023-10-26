415d521542a8c39cc9cebe273f9c4c746957065c
Alberto <betim009@gmail.com>
2023-10-26 20:25:31 -0300

    escrevendo no context

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
+import Footer from './components/Footer'
+import { useState } from 'react';
 import ThemeContext from './context/ThemeContext'
 
 
 function App() {
+  const [pageTheme, setPageTheme] = useState<'dark' | 'light'>('dark');
+
+  function toggleTheme() {
+    setPageTheme(pageTheme === 'dark' ? 'light' : 'dark');
+  }
   return (
-    <ThemeContext.Provider value={{ theme: 'dark' }}>
+    <ThemeContext.Provider value={{ theme: pageTheme, toggleTheme }}>
       <div>
         <h1>Hello World!</h1>
+        <Footer />
       </div>
     </ThemeContext.Provider>
   )
--- vite-project/src/context/ThemeContext.ts
+++ vite-project/src/context/ThemeContext.ts
@@ -2,6 +2,7 @@ import { createContext } from 'react';
 
 type ThemeContextType = {
     theme: 'dark' | 'light';
+    toggleTheme: () => void;
 };
 
 const ThemeContext = createContext({} as ThemeContextType);
